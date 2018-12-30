4374f31f1949bc16b7d45ca8c1ef7b7e127658e7
Zbyněk Vymazal <zbynek@vivino.com>
2018-12-30 10:21:45 +0100

    knot-resolver 3.2.0

---
+++ knot-resolver.rb
@@ -0,0 +1,85 @@
+class KnotResolver < Formula
+  desc "Minimalistic, caching, DNSSEC-validating DNS resolver"
+  homepage "https://www.knot-resolver.cz"
+  url "https://secure.nic.cz/files/knot-resolver/knot-resolver-3.2.0.tar.xz"
+  sha256 "924f1aebad04cacbc4545571239914d2c42e9253784c0df0f391dfad97c59f42"
+  head "https://gitlab.labs.nic.cz/knot/knot-resolver.git"
+
+  bottle do
+    sha256 "7757312aae0148e35c7ad3d89841375c685ae89cb95ee282d539cd96b9374524" => :mojave
+    sha256 "9661b8a1cecb40712f4a853409c2bd2e69affff3fa3628d242a7179fc7c7904a" => :high_sierra
+    sha256 "1ab5d636ca42d5dd4e3c0eb731b303704537a1c3dbff36a3e9fc0916e151377c" => :sierra
+  end
+
+  depends_on "cmocka" => :build
+  depends_on "pkg-config" => :build
+  depends_on "gnutls"
+  depends_on "knot"
+  depends_on "libuv"
+  depends_on "lmdb"
+  depends_on "luajit"
+  depends_on "nettle"
+
+  def install
+    # Since we don't run `make install` or `make etc-install`, we need to
+    # install root.hints manually before running `make check`.
+    cp "etc/root.hints", buildpath
+    (etc/"kresd").install "root.hints"
+
+    %w[all lib-install daemon-install client-install modules-install
+       check].each do |target|
+      system "make", target, "PREFIX=#{prefix}", "ETCDIR=#{etc}/kresd"
+    end
+
+    cp "etc/config.personal", "config"
+    inreplace "config", /^\s*user\(/, "-- user("
+    (etc/"kresd").install "config"
+
+    (etc/"kresd").install "etc/root.hints"
+    (etc/"kresd").install "etc/icann-ca.pem"
+
+    (buildpath/"root.keys").write(root_keys)
+    (var/"kresd").install "root.keys"
+  end
+
+  # DNSSEC root anchor published by IANA (https://www.iana.org/dnssec/files)
+  def root_keys; <<~EOS
+    . IN DS 19036 8 2 49aac11d7b6f6446702e54a1607371607a1a41855200fd2ce1cdde32f24e8fb5
+    . IN DS 20326 8 2 e06d44b80b8f1d39a95c0b0d7c65d08458e880409bbc683457104237c7f8ec8d
+  EOS
+  end
+
+  plist_options :startup => true
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>WorkingDirectory</key>
+      <string>#{var}/kresd</string>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{sbin}/kresd</string>
+        <string>-c</string>
+        <string>#{etc}/kresd/config</string>
+      </array>
+      <key>StandardInPath</key>
+      <string>/dev/null</string>
+      <key>StandardOutPath</key>
+      <string>/dev/null</string>
+      <key>StandardErrorPath</key>
+      <string>#{var}/log/kresd.log</string>
+    </dict>
+    </plist>
+  EOS
+  end
+
+  test do
+    system sbin/"kresd", "--version"
+  end
+end
