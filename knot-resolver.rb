4dfc5a482a60119b6194567be5a0ec6785bcd4e4
Zbyněk Vymazal <zbynek@vivino.com>
2019-01-09 10:22:36 +0100

    Build master branch

--- knot-resolver.rb
+++ knot-resolver.rb
@@ -1,57 +1,51 @@
 class KnotResolver < Formula
-  desc "Minimalistic, caching, DNSSEC-validating DNS resolver"
-  homepage "https://www.knot-resolver.cz"
-  url "https://s3-eu-west-1.amazonaws.com/zbynek.vymazal.eu/knot-resolver-master.tar.gz"
-  sha256 "b4619b081767e84c2653fb11394fa6efa3d7e2857238a84892fab88c37a44cf9"
-  head "https://gitlab.labs.nic.cz/knot/knot-resolver.git"
+  desc 'Minimalistic, caching, DNSSEC-validating DNS resolver'
+  homepage 'https://www.knot-resolver.cz'
+  head 'https://gitlab.labs.nic.cz/knot/knot-resolver.git'
 
-  bottle do
-    sha256 "7757312aae0148e35c7ad3d89841375c685ae89cb95ee282d539cd96b9374524" => :mojave
-    sha256 "9661b8a1cecb40712f4a853409c2bd2e69affff3fa3628d242a7179fc7c7904a" => :high_sierra
-    sha256 "1ab5d636ca42d5dd4e3c0eb731b303704537a1c3dbff36a3e9fc0916e151377c" => :sierra
-  end
-
-  depends_on "cmocka" => :build
-  depends_on "pkg-config" => :build
-  depends_on "gnutls"
-  depends_on "knot"
-  depends_on "libuv"
-  depends_on "lmdb"
-  depends_on "luajit"
-  depends_on "nettle"
+  depends_on 'cmocka' => :build
+  depends_on 'pkg-config' => :build
+  depends_on 'gnutls'
+  depends_on 'knot'
+  depends_on 'libuv'
+  depends_on 'lmdb'
+  depends_on 'luajit'
+  depends_on 'nettle'
 
   def install
     # Since we don't run `make install` or `make etc-install`, we need to
     # install root.hints manually before running `make check`.
-    cp "etc/root.hints", buildpath
-    (etc/"kresd").install "root.hints"
+    cp 'etc/root.hints', buildpath
+    (etc / 'kresd').install 'root.hints'
 
-    %w[all lib-install daemon-install client-install modules-install
-       check].each do |target|
-      system "make", target, "PREFIX=#{prefix}", "ETCDIR=#{etc}/kresd"
+    %w(all lib-install daemon-install client-install modules-install
+       check).each do |target|
+      system 'make', target, "PREFIX=#{prefix}", "ETCDIR=#{etc}/kresd"
     end
 
-    cp "etc/config.personal", "config"
-    inreplace "config", /^\s*user\(/, "-- user("
-    (etc/"kresd").install "config"
+    cp 'etc/config.personal', 'config'
+    inreplace 'config', /^\s*user\(/, '-- user('
+    (etc / 'kresd').install 'config'
 
-    (etc/"kresd").install "etc/root.hints"
-    (etc/"kresd").install "etc/icann-ca.pem"
+    (etc / 'kresd').install 'etc/root.hints'
+    (etc / 'kresd').install 'etc/icann-ca.pem'
 
-    (buildpath/"root.keys").write(root_keys)
-    (var/"kresd").install "root.keys"
+    (buildpath / 'root.keys').write(root_keys)
+    (var / 'kresd').install 'root.keys'
   end
 
   # DNSSEC root anchor published by IANA (https://www.iana.org/dnssec/files)
-  def root_keys; <<~EOS
+  def root_keys
+    <<~EOS
     . IN DS 19036 8 2 49aac11d7b6f6446702e54a1607371607a1a41855200fd2ce1cdde32f24e8fb5
     . IN DS 20326 8 2 e06d44b80b8f1d39a95c0b0d7c65d08458e880409bbc683457104237c7f8ec8d
   EOS
   end
 
-  plist_options :startup => true
+  plist_options startup: true
 
-  def plist; <<~EOS
+  def plist
+    <<~EOS
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
     <plist version="1.0">
@@ -82,6 +76,6 @@ class KnotResolver < Formula
   end
 
   test do
-    system sbin/"kresd", "--version"
+    system sbin / 'kresd', '--version'
   end
 end
